510ace1fb2bcd2806f455cc32437753ddfc92e77
mreliher <66187580+mreliher@users.noreply.github.com>
2026-03-17 09:27:20 -0300

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://mlwdeqpvggbypaaapryh.supabase.co",
+  SUPABASE_URL: "https://webpage-seven-sandy.vercel.app/",
   SUPABASE_ANON_KEY: "sb_publishable_J00-fgduFO6xZpZzPZhbTQ_op1ghHIQ"
 };
